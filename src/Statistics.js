55ac296fec8c096381290b603286dd4a76476d83
aichiang <alexander.chiang@ibm.com>
2019-11-12 17:43:52 -0500

    Refactored code; lifted state up, minor style changes

--- src/Statistics.js
+++ src/Statistics.js
@@ -1,53 +1,19 @@
 import React from 'react';
 import './Statistics.css';
 
+var maxSpeed = 0;
+
 export default class Statistics extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {
-      currentVehicle: {
-        type: 'Bicycle',
-        minSpeed: 10,
-        maxSpeed: 35
-      },
-      speed: 0,
-      distance: 0,
-      time: 0,
-      ticksPerSecond: 60
-    };
-  }
-  componentDidMount() {
-    this.timerID = setInterval(
-      () => this.tick(),
-      1000 / this.state.ticksPerSecond
-    );
-  }
-  componentWillUnmount() {
-    clearInterval(this.timerID);
-  }
-  tick() {
-    const milesToMph = 0.000277778;
-    // Speed decay
-    let speed = Math.max(this.state.currentVehicle.minSpeed, this.state.speed - (1 / this.state.ticksPerSecond));
-    // Travel more distance
-    let distance = this.state.distance + ((this.state.speed * milesToMph) / this.state.ticksPerSecond);
-    let time = this.state.time + (1 / this.state.ticksPerSecond);
-    this.setState({
-      speed: speed,
-      distance: distance,
-      time: time
-    });
-  }
   render() {
     return (
       <div className="component-statistics">
-        <h1>{ this.state.currentVehicle.type }</h1>
-        <h1>Speed: { parseFloat(Math.round(this.state.speed * 100)/100).toFixed(2) } MPH</h1>
-        <h2>Distance: { parseFloat(Math.round(this.state.distance * 100)/100).toFixed(2) } Miles</h2>
-        <h2>Time: { parseFloat(Math.round(this.state.time * 100) / 100).toFixed(2) }</h2>
+        <h1>Stats</h1>
+        <h2>Speed: { parseFloat(Math.round(this.props.speed * 100)/100).toFixed(2) } MPH</h2>
+        <h2>Distance: { parseFloat(Math.round(this.props.distance * 100)/100).toFixed(2) } miles</h2>
+        <h3>Current Vehicle: { this.props.currentVehicle.type }</h3>
+        <h3>Time Elapsed: { parseFloat(Math.round(this.props.time * 100) / 100).toFixed(2) }</h3>
+        <h3>Max Speed: { this.props.speed > maxSpeed ? (maxSpeed = this.props.speed) : maxSpeed } MPH</h3>
       </div>
     );
   }
 }
-
-
