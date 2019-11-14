1337dfe66cabe23f6a918da9129bd54946521d38
mjschleckser <mjschleckser@gmail.com>
2019-11-13 22:24:46 -0500

    Added itemstore, tabs basics

--- src/Store.js
+++ src/Store.js
@@ -1,12 +1,61 @@
 import React from 'react';
+import {storeCatalog} from './StoreItems'
 import './Store.css';
 
 export default class Store extends React.Component {
   render() {
+    // storeCatalog[0].func();
     return (
-      <div className="component-store">
-      	This is where the store will be.
+      <div>
+        <TabsContainer>
+          <Tab id="Store" active="true">{storeCatalog[0].name}</Tab>
+          <Tab id="Upgrades">asdgf</Tab>
+          <Tab id="Achievements">zxcvbn</Tab>
+        </TabsContainer>
       </div>
     );
+  }
+}
+
+class TabsContainer extends React.Component {
+  activateTab(){
+
+  }
+
+  render() {
+    // TabsContainer creates a button for each Tab element within it
+    var tabHeaders = this.props.children.map(function(child){
+        if(child.type === Tab){
+          return <button className="Tab" onClick={alert}>{child.props.id}</button>
+        }
+    });
+    return (
+      <div className="tabs">
+        <div className="tabHeaders">
+          {tabHeaders}
+        </div>
+        <div className="tabContent">
+          {this.props.children}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Tab extends React.Component {
+  constructor(props){
+    super(props);
+    // this.activate = this.activate.bind(this);
+  }
+  activate(){
+    alert('!!!');
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
   }
 }
---
+++ src/StoreItems.js
@@ -0,0 +1,12 @@
+export var storeCatalog = [
+  {
+      name: 'tricycle',
+      cost: 4,
+      func: function(){alert("Bike")},
+  }, {
+      name: 'cycle',
+      cost: 7,
+      active: true,
+      func: function(){alert("Cycle")},
+  }
+];
