de624522562d0fa1b5fd30e257491aa106f82f3a
mjschleckser <mjschleckser@gmail.com>
2019-12-09 23:12:33 -0500

    Tooltips - work inprogress

--- src/Item.js
+++ src/Item.js
@@ -16,7 +16,8 @@ export default class Item extends React.Component {
   render() {
   	const path = `./assets/${ this.props.item.name.replace(' ','_').toLowerCase() }.png`;
     return (
-      <div className="component-item">
+      <div className="component-item tooltip">
+        <div className="tooltiptext"> <span> { this.props.item.description } 23 </span> </div>
         <div className="item" onClick={ this.purchaseItem } onMouseEnter={ this.describeItem }>
         	<img src={ path } alt={ this.props.item.name } />
         	<div className="item-cost">{ this.props.item.cost }</div>
